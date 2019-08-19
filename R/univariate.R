f7798d5661d9349ba86a44a27bf718b5a454b861
Ethan Heinzen <heinzen.ethan@mayo.edu>
2019-08-19 13:33:10 -0500

    Add cutoff

--- R/univariate.R
+++ R/univariate.R
@@ -14,7 +14,7 @@ calc_outlier <- function(x, cutoff = 0.01)
   }
 }
 
-univariate <- function(dat, cutoff)
+univariate <- function(dat, cutoff = 0.05)
 {
   ## Calculate missings (counts and percents)
   nmiss <- colSums(is.na(dat))
