4485988831bccd1aab98a5abc83291c21dc54d26
Ethan Heinzen <heinzen.ethan@mayo.edu>
2019-08-20 12:56:57 -0500

    Add dq.R

---
+++ R/dq.R
@@ -0,0 +1,21 @@
+#' The dq Package
+#'
+#' Tools for Data Quality.
+#'
+#' @section Functions:
+#'
+#' \code{\link{dq_univariate}}
+#'
+#' \code{\link{dq_pairwise}}
+#'
+#' \code{\link{dq_pca}}
+#'
+#' \code{\link{dq_multivariate}}
+#' @docType package
+#' @name dq
+#'
+NULL
+
+#' @useDynLib dq
+#' @importFrom Rcpp sourceCpp
+NULL
